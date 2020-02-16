977008489e4dc82ec5d074fe45921e6cdcde3e85
shuja990 <shujaali1234@gmail.com>
2020-02-16 14:08:32 +0500

    Updates

---
+++ precache-manifest.bd6a7a18685bf0416843d998e622eb03.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4fccd009e18403448e6bab6526c379c",
+    "url": "/shuja990.github.io/roboFriends/index.html"
+  },
+  {
+    "revision": "b2501a153361e7106eb0",
+    "url": "/shuja990.github.io/roboFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "24630111be30749a56a9",
+    "url": "/shuja990.github.io/roboFriends/static/css/main.e6db17d6.chunk.css"
+  },
+  {
+    "revision": "b2501a153361e7106eb0",
+    "url": "/shuja990.github.io/roboFriends/static/js/2.e4e98ab1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/shuja990.github.io/roboFriends/static/js/2.e4e98ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24630111be30749a56a9",
+    "url": "/shuja990.github.io/roboFriends/static/js/main.60ba5def.chunk.js"
+  },
+  {
+    "revision": "01f2afadd5d2335ed805",
+    "url": "/shuja990.github.io/roboFriends/static/js/runtime-main.cf7e950a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/shuja990.github.io/roboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
